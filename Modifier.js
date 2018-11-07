5b7944c834e50108c61bf1bb7758e2c0bf1545ac
Kasandra Wolf <wolf.kasandra@gmail.com>
2018-11-06 20:24:47 -0700

    with Mark'

---
+++ Modifier.js
@@ -0,0 +1,7 @@
+export default class Modifier {
+  constructor(name, modifier, description) {
+    this.name = name;
+    this.modifier = modifier;
+    this.description = description;
+  }
+}
--- game.js
+++ game.js
@@ -1,20 +1,47 @@
+
+//Underscores are for Java this should be maxMods
 const MAX_MODS = 2;
 
 let bird = { // target
   name: 'Dodger Duck',
   health: 100,
   hits: 0,
-  items: []
+  items: [],
+  // attacks: {
+  //   slingshot: 1,
+  //   blowgun: 5,
+  //   crossbow: 10
+  // }
 }
+//Now that you know how to, these would be good as classes
+
 let rain = { name: 'rain', modifier: 0.1, bool: false, description: 'Reduce velocity 10%' }
 let wind = { name: 'wind', modifier: 0.25, bool: false, description: 'Reduce projectile velocity by 25%' }
 let food = { name: 'fish', modifier: -0.25, bool: false, description: 'Make Dodger stronger' }
-let evade = false;
+let items = { rain, wind, food }
 
-let items = { rain, wind, food, evade }
+/*
+let items = {
+  rain: new Item(...),
+  wind: new Item(...)
+}
+*/
 
+
+//You can consolidate the next three functions into one, and you can simplify the function just a bit with a change of logic on the if, when you are full on items, pop one off first, as you will always add one
+/*
+take in a string of the type to affect ('rain', 'wind', 'food')
+function applyMod(type){
+  if (bird.items.length == MAX_MODS) {
+    bird.items.shift();
+  }
+  bird.items.push(items[type]);
+  }
+}
+
+*/
 function changeWind() {
-  wind.bool = !wind.bool; // toggle wind
+  //bool never seems to be used for anything?
   if (bird.items.length < MAX_MODS) {
     bird.items.push(wind);
   } else {
@@ -23,7 +50,6 @@ function changeWind() {
   }
 }
 function changeRain() {
-  rain.bool = !rain.bool; // toggle wind
   if (bird.items.length < MAX_MODS) {
     bird.items.push(rain);
   } else {
@@ -32,7 +58,6 @@ function changeRain() {
   }
 }
 function feedDuck() {
-  food.bool = !food.bool; // toggle wind
   if (bird.items.length < MAX_MODS) {
     bird.items.push(food);
   } else {
@@ -41,7 +66,13 @@ function feedDuck() {
   }
 
 }
+//taking in some user applied damage is dangerous as they can pass whatever value they want and the function just accepts it
+// it is safer to use an object see line 12 for modifications to make this function work
+
+//change damage to damageType, and in your HTML you can change it to the string of the attack type
 function shoot(damage) {
+  //this line becomes
+  //bird.health -= bird.attacks[damageType] * addMods()
   bird.health -= damage * addMods();
   document.getElementById('bang').className = ''; //removes hide-element class
   setTimeout(() => {
@@ -53,9 +84,12 @@ function shoot(damage) {
   bird.hits++;
   update();
 }
+
+//I think this funciton is not doing anything?
 function drawbang() {
   document.getElementById('')
 }
+
 function healBird() {
   bird.health += 3;
   if (bird.health > 100)
@@ -81,5 +115,4 @@ function resetGame() {
   bird.items = [];
   update();
 }
-
 update();
